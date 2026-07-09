c28798280cf602cf02fd4afc8304740a81534978
Mikus Rakis <mikus.rakis@gmail.com>
2026-07-09 12:32:13 +0300

    wip

--- src/main/java/lv/bootcamp/shelter/controller/AnimalPageController.java
+++ src/main/java/lv/bootcamp/shelter/controller/AnimalPageController.java
@@ -1,25 +1,40 @@
 package lv.bootcamp.shelter.controller;
 
+import lv.bootcamp.shelter.form.AnimalForm;
+import lv.bootcamp.shelter.service.AnimalService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AnimalPageController {
+    private final AnimalService animalService;
+
+    public AnimalPageController(AnimalService animalService) {
+        this.animalService = animalService;
+    }
+
     public String index() {
         return "animals";
     }
-    @GetMapping("/animals")
-    public String animals() {
 
+    @GetMapping("/animals")
+    public String listAnimals(Model model) {
+        model.addAttribute("animals", animalService.findAll());
         return "animals";
     }
+
     @GetMapping("/animals/new")
-    public String newAnimal() {
+    public String newAnimal(Model model) {
+        model.addAttribute("form", new AnimalForm(null, null, null, null, null,null));
         return "animals-new";
     }
+
     @PostMapping("/animals")
-    public String createAnimal() {
+    public String createAnimal(@ModelAttribute AnimalForm form) {
+        animalService.createFromForm(form);
         return "redirect:animals";
     }
 }
