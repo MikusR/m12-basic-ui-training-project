67947ad30d507c4ac2fd4dc3a0f7bbbadf0f8c31
Mikus Rakis <mikus.rakis@gmail.com>
2026-07-09 22:29:39 +0300

    add basic validation

--- src/main/java/lv/bootcamp/shelter/controller/AnimalPageController.java
+++ src/main/java/lv/bootcamp/shelter/controller/AnimalPageController.java
@@ -1,6 +1,7 @@
 package lv.bootcamp.shelter.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lv.bootcamp.shelter.form.AnimalForm;
 import lv.bootcamp.shelter.model.AnimalType;
@@ -8,6 +9,7 @@ import lv.bootcamp.shelter.service.AnimalService;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,13 +32,18 @@ public class AnimalPageController {
 
     @GetMapping("/animals/new")
     public String newAnimal(Model model, HttpServletRequest request) {
+        if (!request.isUserInRole("ADMIN")){return "redirect:animals";}
         model.addAttribute("types", AnimalType.values());
         model.addAttribute("form", new AnimalForm(null, null, null, null, null, null));
         return "animals-new";
     }
 
     @PostMapping("/animals")
-    public String createAnimal(@ModelAttribute AnimalForm form) {
+    public String createAnimal(@ModelAttribute("form") @Valid AnimalForm form, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("types", AnimalType.values());
+            return "animals-new";
+        }
         animalService.createFromForm(form);
         return "redirect:animals";
     }
